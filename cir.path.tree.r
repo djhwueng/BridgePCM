a22ebf2c0c17317ef6d2c5939d9234a23e742be1
Tony Jhwueng <djhwueng@umail.iu.edu>
2017-09-23 10:55:35 +0800

    cit path tree better look

--- cir.path.tree.r
+++ cir.path.tree.r
@@ -41,53 +41,58 @@ sim.cir.path.tree<-function(model.params,phy=phy,N=N,root=root){
   return(list(path.objects=path.objects,sim.node.data=sim.node.data))
   }
 
-  plot.history<-function(phy=phy,path.data=path.data,main=main){
+
+  plot.history.dt<-function(phy=phy,path.data=path.data,main=main){
       ntips<-length(phy$tip.label)
-   	  edge.number<-dim(phy$edge)[1]
+      edge.number<-dim(phy$edge)[1]
       x.start<-array(0,c(edge.number,1))
       x.end<-array(0,c(edge.number,1))
       step<-array(0,c(edge.number,1))
-   	  anc.des.start.end<-cbind(phy$edge,step,x.start, x.end)
-   	  colnames(anc.des.start.end)<-c("anc","des","step","x.start","x.end")
+      anc.des.start.end<-cbind(phy$edge,step,x.start, x.end)
+      colnames(anc.des.start.end)<-c("anc","des","step","x.start","x.end")
       anc.des.start.end<-apply(anc.des.start.end,2,rev)
-   	  anc.des.start.end<-data.frame(anc.des.start.end)
+      anc.des.start.end<-data.frame(anc.des.start.end)
       anc<- anc.des.start.end$anc
       des<- anc.des.start.end$des
-   	  for(edgeIndex in 1:edge.number){
+      for(edgeIndex in 1:edge.number){
          path<-unlist(path.data$path.objects[edgeIndex])
          anc.des.start.end$step[edgeIndex]<-length(path)
-   	 		 }
+         }
       for(edgeIndex in 1:edge.number){
-  		  if(anc[edgeIndex]== Ntip(phy)+1){
+        if(anc[edgeIndex]== Ntip(phy)+1){
            anc.des.start.end$x.start[edgeIndex]<- 1+ ceiling(nodeheight(phy,node=anc.des.start.end$anc[edgeIndex]))
            }else{
-           			anc.des.start.end$x.start[edgeIndex]<- ceiling(nodeheight(phy,node=anc.des.start.end$anc[edgeIndex]))
-  			 				}
-  				 anc.des.start.end$x.end[edgeIndex]<- 1 + ceiling(nodeheight(phy,node=anc.des.start.end$des[edgeIndex]))
+                anc.des.start.end$x.start[edgeIndex]<- ceiling(nodeheight(phy,node=anc.des.start.end$anc[edgeIndex]))
+                }
+           anc.des.start.end$x.end[edgeIndex]<- 1 + ceiling(nodeheight(phy,node=anc.des.start.end$des[edgeIndex]))
           }
-      main="cir"
       plot( NA,type="l",xlim=c(1,ceiling(get.rooted.tree.height(phy))+1 ),ylim=c(min(unlist(path.data)), max(unlist(path.data))),ylab="Trait value", xlab="Time Steps",main=main)
       anc.des.start.end
       for(edgeIndex in 1:edge.number){
-        edgeIndex <- 1
+
         path<-unlist(path.data$path.objects[edgeIndex])
         #starting.x<-ceiling(nodeheight(phy,node=anc[edgeIndex]))
         x.start <-  anc.des.start.end$x.start[edgeIndex]
         x.end   <-  anc.des.start.end$x.end[edgeIndex]
         gap <- round(anc.des.start.end$step[edgeIndex]/( x.end-x.start))
         #points((starting.x+1): (starting.x+length(path)),path[seq(1, anc.des.start.end$step[edgeIndex], by = gap)],type="l")
-        points(  (anc.des.start.end$x.start[edgeIndex]: anc.des.start.end$x.end[edgeIndex]),
+        point.to.use<-(anc.des.start.end$x.start[edgeIndex]: anc.des.start.end$x.end[edgeIndex])
+        sample.path<-path[seq(1,  anc.des.start.end$step[edgeIndex],by=gap)]
 
+        if(length(point.to.use)!=length(sample.path)){
+            if(length(point.to.use) > length(sample.path)){
+              point.to.use<-point.to.use[1:length(sample.path)]
+            }else{
+              sample.path<-sample.path[1:length(point.to.use)]
+              }
+            }
 
-        path[seq(1, anc.des.start.end$step[edgeIndex], by = gap)  # we need to find a better way to sample this and get right number with x - axis
+        points(point.to.use , sample.path, type="l")
+        #path[seq(1, anc.des.start.end$step[edgeIndex], by = gap)  # we need to find a better way to sample this and get right number with x - axis
          #path[sample(    )    ]  LOOK UP SAMPLE FOR GOOD
-
-
-        ],type="l")
           }
   #    abline(v=ceiling(get.rooted.tree.height(phy))+1)
-  	  }#end of plot history
-
+      }#end of plot history
 
 T<-1
 N<-3000
@@ -108,7 +113,10 @@ while(min(phy$edge.length)<min.length){
   phy$edge.length<-1.005*phy$edge.length
   }
 phy$edge.length
-plot(phy)
+
 path.data<-sim.cir.path.tree(model.params,phy=phy,N=N,root=root)
 #print(path.data)
-plot.history(phy=phy,path.data=path.data,main="CIR")
+
+par(mfrow=c(1,2))
+plot.history.dt(phy=phy,path.data=path.data,main="CIR")
+plot(phy)
